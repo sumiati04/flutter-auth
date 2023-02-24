c2823ab765597a656954cde33cb57fd96b50330a
sumiati04 <ss6289285@gmail.com>
2023-02-24 15:50:09 +0700

    auth pertama

--- lib/app/modules/dashboard/controllers/dashboard_controller.dart
+++ lib/app/modules/dashboard/controllers/dashboard_controller.dart
@@ -1,7 +1,42 @@
+import 'dart:convert';
+
 import 'package:get/get.dart';
 
+import '../../../data/entertainment_response.dart';
+import '../../../data/headline_response.dart';
+import '../../../data/sports_response.dart';
+import '../../../data/technology_response.dart';
+import '../../../utils/api.dart';
+
 class DashboardController extends GetxController {
-  //TODO: Implement DashboardController
+  final _getConnect = GetConnect();
+  Future<HeadlineResponse> getHeadline() async {
+    //memanggil fungsi getConnect untuk melakukan request ke BaseUrl.headline
+    final response = await _getConnect.get(BaseUrl.headline);
+    //mengembalikan data response dalam bentuk HeadlineResponse setelah di-decode dari JSON
+    return HeadlineResponse.fromJson(jsonDecode(response.body));
+  }
+
+  Future<EntertainmentResponse> getEntertainment() async {
+    //memanggil fungsi getConnect untuk melakukan request ke BaseUrl.headline
+    final entertainment = await _getConnect.get(BaseUrl.entertainment);
+    //mengembalikan data response dalam bentuk HeadlineResponse setelah di-decode dari JSON
+    return EntertainmentResponse.fromJson(jsonDecode(entertainment.body));
+  }
+
+  Future<SportsResponse> getSports() async {
+    //memanggil fungsi getConnect untuk melakukan request ke BaseUrl.headline
+    final sports = await _getConnect.get(BaseUrl.sports);
+    //mengembalikan data response dalam bentuk HeadlineResponse setelah di-decode dari JSON
+    return SportsResponse.fromJson(jsonDecode(sports.body));
+  }
+
+  Future<TechnologyResponse> getTechnology() async {
+    //memanggil fungsi getConnect untuk melakukan request ke BaseUrl.headline
+    final technology = await _getConnect.get(BaseUrl.technology);
+    //mengembalikan data response dalam bentuk HeadlineResponse setelah di-decode dari JSON
+    return TechnologyResponse.fromJson(jsonDecode(technology.body));
+  }
 
   final count = 0.obs;
   @override
